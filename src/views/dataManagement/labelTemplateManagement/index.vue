0fc2ebcf3bf99fa830efac141dcac2dced55fccf
xqk <1379502663@qq.com>
2026-04-19 09:32:03 +0800

    test

---
+++ src/views/dataManagement/labelTemplateManagement/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hbt-app-container">
+    <el-card shadow="never" class="hbt-card-container">
+      <h2>标签模板管理</h2>
+      <!-- TODO: Form and Table will be here -->
+    </el-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+// 暂时为空，留作后续API对接和逻辑开发
+</script>
+
+<style scoped>
+</style>
